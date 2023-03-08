79a8178550a9664e8a1a1779f90bbc23892fc877
Jialun <tobeabellwether@gmail.com>
2023-03-08 22:05:25 +0100

    add project

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'CNY', 6, '');
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'EUR', 'CNY', 7, '');
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'GBP', 'CNY', 9, '');
+
